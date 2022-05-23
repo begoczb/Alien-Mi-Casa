b1850efce1f293b8c4f36ff3ebf19a34586b0a65
begoczb <begocz.31@gmail.com>
2022-05-23 17:40:31 +0200

    add css hide/show container

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
-const myGame = new Game();
+const rulesBtn = document.querySelector(".rules");
+console.log(rulesBtn);
+const rules = document.querySelector(".rules-container");
+console.log(rules);
+
+rulesBtn.addEventListener("click", () => {
+  if (!rules.classList.contains("show")) {
+    rules.classList.add("show");
+  } else {
+    rules.classList.remove("show");
+  }
+});
