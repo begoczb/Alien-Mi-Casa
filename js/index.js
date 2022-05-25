d348ef2fe6c4c2ec6df17b8167ca402642b08d60
begoczb <begocz.31@gmail.com>
2022-05-25 15:14:02 +0200

    add button css + cleaned files/folders

--- js/index.js
+++ js/index.js
@@ -1,12 +1,33 @@
+const myGame = new Game();
+
+const backgroundMusic = document.querySelector("audio");
+const soundBtn = document.querySelector(".sound");
+const soundImg = document.querySelector(".sound img");
+
 const rulesBtn = document.querySelector(".rules");
-console.log(rulesBtn);
 const rules = document.querySelector(".rules-container");
+
+backgroundMusic.play();
+
+soundBtn.onclick = () => {
+  if (soundBtn.classList.contains("on")) {
+    backgroundMusic.pause();
+    soundImg.src = "./src/images/sound_icon.svg";
+    soundBtn.classList.replace("on", "off");
+  } else if (soundBtn.classList.contains("off")) {
+    backgroundMusic.play();
+    soundImg.src = "./src/images/no_sound_icon.svg";
+    soundBtn.classList.replace("off", "on");
+  }
+};
+
+console.log(rulesBtn);
 console.log(rules);
 
 rulesBtn.addEventListener("click", () => {
-  if (!rules.classList.contains("show")) {
-    rules.classList.add("show");
+  if (rules.classList.contains("hidden")) {
+    rules.classList.remove("hidden");
   } else {
-    rules.classList.remove("show");
+    rules.classList.add("hidden");
   }
 });
